b70a8cd22331838145bbc29a4b19f6369a373f06
Patrick Ineichen <github@frokonet.ch>
2018-05-04 15:19:11 +0200

    Removed Giftcard sample

--- source/SimpleDomain.GiftcardSample.Contracts/Commands.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace GiftcardSample.Commands
-open SimpleDomain
-open System
-
-type CreateGiftcard = { CardNumber : int; InitialBalance : decimal; ValidUntil : DateTime } with interface ICommand
-type ActivateGiftcard = { CardId : Guid; } with interface ICommand
-type RedeemGiftcard = { CardId : Guid; Amount : decimal } with interface ICommand
-type LoadGiftcard = { CardId : Guid; Amount : decimal } with interface ICommand
